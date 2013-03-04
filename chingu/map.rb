a4d825954c0821ada2142d9965d2f89a50241a1d
Stuart Nelson <stuartnelson3@gmail.com>
2013-03-04 11:01:09 -0600

    momentum working, need to refactor

--- chingu/map.rb
+++ chingu/map.rb
@@ -7,6 +7,8 @@ class Game < Chingu::Window
   def initialize
     super(640, 480, false)
 
+    @map = Map.new(self, "media/game_map.txt")
+
     @jm = JumpingMan.create(image: Image["jumping_man.png"])
     @jm.x = width / 2
     @jm.y = height - (@jm.height/2)
@@ -20,8 +22,62 @@ class Game < Chingu::Window
 
   def draw
     fill(Color::WHITE)
+    @map.draw
     self.caption = "JM dy: #{@jm.dy}; JM y: #{@jm.y}"
     super
+  end
+end
+
+# Map class holds and draws tiles and gems.
+module Tiles
+  Grass = 0
+  Earth = 1
+end
+class Map
+  include Gosu
+  attr_reader :width, :height, :gems
+  
+  def initialize(window, filename)
+    # Load 60x60 tiles, 5px overlap in all four directions.
+    @tileset = Image.load_tiles(window, "media/CptnRuby Tileset.png", 60, 60, true)
+
+    lines = File.readlines(filename).map { |line| line.chomp }
+    @height = lines.size
+    @width = lines[0].size
+    @tiles = Array.new(@width) do |x|
+      Array.new(@height) do |y|
+        case lines[y][x, 1]
+        when '"'
+          Tiles::Grass
+        when '#'
+          Tiles::Earth
+        when 'x'
+          nil
+        else
+          nil
+        end
+      end
+    end
+  end
+  
+  def draw
+    # Very primitive drawing function:
+    # Draws all the tiles, some off-screen, some on-screen.
+    @height.times do |y|
+      @width.times do |x|
+        tile = @tiles[x][y]
+        if tile
+          # Draw the tile with an offset (tile images have some overlap)
+          # Scrolling is implemented here just as in the game objects.
+          @tileset[tile].draw(x * 50 - 5, y * 50 - 5, 0)
+        end
+      end
+    end
+  end
+  
+  # Solid at a given pixel position?
+  def solid?(x, y)
+    y < 0 || @tiles[x / 50][y / 50]
   end
 end
 
@@ -74,7 +130,7 @@ class JumpingMan < Chingu::GameObject
   end
 
   def hori_decay
-    modifier = airbourne? ? 1 : 0.7
+    modifier = airbourne? ? 1 : 0.5
     self.dx = VelocityDecay.horizontal(dx, modifier)
   end
 
@@ -87,8 +143,7 @@ class JumpingMan < Chingu::GameObject
     @x += dx
 
     if grounded?
-      self.dy *= -1 # bounce on impact
-      # self.dy = 0
+      # self.dy *= -1 # bounce on impact
       @y = ($window.height - height/2)
     end
 
