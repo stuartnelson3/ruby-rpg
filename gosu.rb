15d87ff905d1284781d2176ed34d9a14a5cb970b
Stuart Nelson <stuartnelson3@gmail.com>
2013-04-23 14:57:24 -0500

    testing key

--- gosu.rb
+++ gosu.rb
@@ -10,26 +10,26 @@ class Player
   def warp(x, y)
     @x, @y = x, y
   end
-  
+
   def turn_left
     @angle -= 4.5
   end
-  
+
   def turn_right
     @angle += 4.5
   end
-  
+
   def accelerate
     @vel_x += Gosu::offset_x(@angle, 0.5)
     @vel_y += Gosu::offset_y(@angle, 0.5)
   end
-  
+
   def move
     @x += @vel_x
     @y += @vel_y
     @x %= 640
     @y %= 480
-    
+
     @vel_x *= 0.95
     @vel_y *= 0.95
   end
